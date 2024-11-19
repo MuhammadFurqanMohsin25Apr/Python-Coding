d693ca7e57b6c8612fec365dfa789cbbf76cd6da
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-11-19 15:37:58 -0800

    file3

---
+++ file3.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+num1=64
+sr1=num1**0.5
+print("the square root of num1 is:",sr1)
+num2=int(input("Enter a number: "))
+sr2=num2**0.5
+
+num3=int(input("Enter a number: "))
+print("the square root of num2 is:",sr2)
+print("the square root of num3 is:",sqrt(num3))
